aa75a6533481e3f3761f4f815463e84fd094fe98
{leoalonsoyvega} <{leoalonsoyvega@gmail.com}>
2015-04-24 19:36:27 -0300

    commit test partes_tablero

--- tenis/spec/partes_tablero_spec.rb
+++ tenis/spec/partes_tablero_spec.rb
@@ -7,4 +7,11 @@ describe 'PartesTablero' do
 		partes.elem1.should be 0
 		partes.elem2.should be 0
 	end
+
+	it 'getElem(1) devuelve 0 y getElem(2) devuelve 0' do
+
+		partes + PartesTablero.new
+		partes.getElem(1).should be 0
+		partes.getElem(2).should be 0
+	end
 end
