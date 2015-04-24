26b99251506368debd7be6c0f5829bb92311765d
{leoalonsoyvega} <{leoalonsoyvega@gmail.com}>
2015-04-24 19:48:42 -0300

    3 test creado partes_tablero

--- tenis/spec/partes_tablero_spec.rb
+++ tenis/spec/partes_tablero_spec.rb
@@ -14,4 +14,14 @@ describe 'PartesTablero' do
 		partes.getElem(1).should be 0
 		partes.getElem(2).should be 0
 	end
+
+	it 'set Elem(1,30) devuelve 30 en el primer elem1 y setElem(2,15) devuelve 15 en el segundo elem2' do
+		partes = PartesTablero.new
+		partes.elem1.should be 0
+		partes.elem2.should be 0
+		partes.setElem(1,30)
+		partes.setElem(2,15)
+		partes.elem1.should be 30
+		partes.elem2.should be 15
+	end
 end
