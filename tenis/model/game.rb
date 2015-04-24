af87080d698fe745a1fb7af634a71c3ed267d75e
{leoalonsoyvega} <{leoalonsoyvega@gmail.com}>
2015-04-24 20:41:18 -0300

    2 test correcto game_spec

--- tenis/model/game.rb
+++ tenis/model/game.rb
@@ -7,4 +7,13 @@ class Game < PartesTablero
 	end
 
 	attr_reader :set
+
+	def next(num)
+		self.setElem(num, self.getElem(num) + 1)
+		if self.getElem(num) == 6
+			self.reset()
+			self.set.next(num)
+		end
+	end
+	
 end
