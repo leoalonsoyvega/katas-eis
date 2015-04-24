7053106f61658dc8d83a21ec94307ca7d22aa0d1
{leoalonsoyvega} <{leoalonsoyvega@gmail.com}>
2015-04-24 19:29:14 -0300

    1 test partes_tablero correcto

--- tenis/model/partes_tablero.rb
+++ tenis/model/partes_tablero.rb
@@ -1,7 +1,7 @@
 class PartesTablero
 	def initialize
-		@elem1,@elem2 = 0,0
+		@elem1, @elem2 = 0,0
 	end
 
-	attr_accessor = :elem1, :elem2
+	attr_accessor :elem1, :elem2
 end
--- tenis/spec/partes_tablero_spec.rb
+++ tenis/spec/partes_tablero_spec.rb
@@ -1,10 +1,10 @@
 require 'rspec'
-require_relative "../model/partes_tablero.rb"
+require_relative '../model/partes_tablero.rb'
 
 describe 'PartesTablero' do
 	it 'initialize' do
-		partes_tablero = PartesTablero.new
-		partes_tablero.elem1.should be 0
-		partes_tablero.elem2.should be 0
+		partes = PartesTablero.new
+		partes.elem1.should be 0
+		partes.elem2.should be 0
 	end
 end
